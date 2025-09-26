2b68526b47be392f14a957266cddd47559978443
Audrey Su <audreysu@berkeley.edu>
2025-09-26 14:24:51 -0700

    fixed data analysis

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -22,20 +22,20 @@ def load_students(filename):
 
 def calculate_average_grade(students):
     total_grade = 0
-    for student in students[1:]:
+    for student in students:
         total_grade += int(student["grade"])
     average = total_grade / (len(students) -1 )
     return average
 
 def count_math_students(students):
-    count = sum(1 for student in students[1:] if student["subject"] =='Math')
+    count = sum(1 for student in students if student["subject"] =='Math')
     return count
 
 def generate_report():
     students = load_students('data/students.csv')
     avg_grade = calculate_average_grade(students)
     math_count = count_math_students(students)
-    return f"Average grade of students: {avg_grade:.1f} and number of students enrolled in Math: {math_count}"
+    return f"Analysis: Average grade of students: {avg_grade:.1f} and number of students enrolled in Math: {math_count}"
 
 def save_report(report, filename):
     with open(filename, 'w') as f:
