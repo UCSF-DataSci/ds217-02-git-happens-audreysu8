80c6b121613b1bc8b741f77cfc09560166739555
Audrey Su <audreysu@berkeley.edu>
2025-09-27 20:05:47 -0700

    remove fibonacci

--- fibonacci_iterative.py
+++
@@ -1,8 +0,0 @@
-def fibonacci_iterative(n):
-    """Return the nth Fibonacci number using an iterative approach."""
-    if n < 0:
-        raise ValueError("n must be non-negative")
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-    return a
