31c6f1cb5e8275e5e79fd18332131bc22966aa5c
Audrey Su <audreysu@berkeley.edu>
2025-09-28 19:38:55 -0700

    I made some changes to the starter function files. I decided to push my changes to them to GitHub.

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,6 +1,12 @@
-# TODO: Need to implement main script
+# TODO: Need to add data analysis here
+def sample_function():
+    pass
+def sample_function_2():
+    pass
+
+# TODO: Then add a main function to orchestrate analysis
 def main():
-    print("Hello, world!")
+    pass
 
 if __name__ == "__main__":
     main()
--- src/data_analysis_functions.py
+++ src/data_analysis_functions.py
@@ -1,5 +1,12 @@
-# TODO: Need to add utility functions
+# TODO: Need to add extra data analysis functions here
 def sample_function():
     pass
 def sample_function_2():
     pass
+
+# TODO: Then add a main function to orchestrate analysis
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
