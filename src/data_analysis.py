c7c900c1af7fd9a8bf2b6aa144c3e3a55a7f3214
Audrey Su <audreysu@berkeley.edu>
2025-09-28 19:08:10 -0700

    fixed some errors here

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -37,22 +37,12 @@ def count_math_students(students):
     count = sum(1 for student in students if student["subject"] =='Math')
     return count
 
-def count_science_students(students):
-    count = sum(1 for student in students if student["subject"] =='Science')
-    return count
-
-def count_history_students(students):
-    count = sum(1 for student in students if student["subject"] =='History')
-    return count
-
 def generate_report():
     students = load_students('data/students.csv')
     total_students = calculate_total_studens(students)
     avg_grade = calculate_average_grade(students)
     math_count = count_math_students(students)
-    science_count = count_science_students(students)
-    history_count = count_history_students(students)
-    return f"Analysis: Average grade of students: {avg_grade:.1f}.\nTotal number of students: {total_students}.\nNumber of students enrolled in Math: {math_count}. Number of students enrolled in Science: {science_count}. Number of students enrolled in History: {history_count}."
+    return f"Analysis: Average grade of students: {avg_grade:.1f}.\nTotal number of students: {total_students}.\nNumber of students enrolled in Math: {math_count}."
 
 def save_report(report, filename):
     with open(filename, 'w') as f:
