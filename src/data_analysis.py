8a592ac6a5223020574c1c0f9031c5db2f8729fa
Audrey Su <audreysu@berkeley.edu>
2025-09-30 19:13:33 -0700

    I modified the templates for data_analysis_functions.py and data_analysis.py

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,11 +1,41 @@
-# TODO: Need to add data analysis here
-def sample_function():
-    pass
-def sample_function_2():
+def load_students(filename):
+    """Load student data from CSV."""
+    # TODO: Open file, read lines, skip header
+    # TODO: Split each line by comma
+    # TODO: Return list of student data
     pass
 
-# TODO: Then add a main function to orchestrate analysis
+def calculate_average_grade(students):
+    """Calculate average grade."""
+    # TODO: Sum all grades
+    # TODO: Divide by number of students
+    pass
+
+def calculate_total_students(students):
+    """Calculate total number of students."""
+    # TODO: Sum all students
+
+def count_math_students(students):
+    """Count students in Math."""
+    # TODO: Count students where subject is Math
+    pass
+
+def generate_report(total, average, math_count):
+    """Generate report string."""
+    # TODO: Create formatted string with results
+    # TODO: Use f-strings with .1f for decimals
+    pass
+
+def save_report(report, filename):
+    """Save report to file."""
+    # TODO: Create output directory if needed
+    # TODO: Write report to file
+    pass
+
 def main():
+    # TODO: Load data
+    # TODO: Calculate statistics
+    # TODO: Generate and save report
     pass
 
 if __name__ == "__main__":
